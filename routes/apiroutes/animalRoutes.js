c7fccb2eb2092bd2ebe789e99552b6999a58f862
BTV <horsfalm@gmail.com>
2022-01-14 12:41:06 +1100

    add zookeeper entry

---
+++ routes/apiroutes/animalRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.post('/animals', (req, res) => {
+    req.body.id = animals.length.toString();
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('Then animal is not properly formatted.');
+    } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
+});
+
+router.get('/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+    res.send(404);
+    }
+});
+
+module.exports = router;
